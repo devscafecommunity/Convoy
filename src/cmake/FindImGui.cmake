3d0c7440e2f58f50890f6451f1edabbaac066b3f
LyeZinho <pedrokalebdej1@gmail.com>
2026-04-09 15:23:17 +0100

    fix: FindImGui sets ImGui_FOUND and guards against missing GLFW/OpenGL

--- src/cmake/FindImGui.cmake
+++ src/cmake/FindImGui.cmake
@@ -1,14 +1,30 @@
-add_library(ImGui INTERFACE)
-target_include_directories(ImGui INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui)
-target_sources(ImGui INTERFACE
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/imgui.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/imgui_demo.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/imgui_draw.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/imgui_widgets.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/imgui_tables.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/backends/imgui_impl_opengl3.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui/backends/imgui_impl_glfw.cpp
-)
-target_link_libraries(ImGui INTERFACE OpenGL::OpenGL GLFW::GLFW)
+set(IMGUI_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/imgui")
 
-add_library(ImGui::ImGui ALIAS ImGui)
+# Guard: ImGui depends on GLFW and OpenGL already being found
+if(NOT GLFW3_FOUND OR NOT OpenGL_FOUND)
+  set(ImGui_FOUND FALSE)
+  return()
+endif()
+
+if(EXISTS "${IMGUI_DIR}/imgui.h" AND
+   EXISTS "${IMGUI_DIR}/backends/imgui_impl_opengl3.h" AND
+   EXISTS "${IMGUI_DIR}/backends/imgui_impl_glfw.h")
+  if(NOT TARGET ImGui)
+    add_library(ImGui INTERFACE)
+    target_include_directories(ImGui INTERFACE "${IMGUI_DIR}")
+    target_sources(ImGui INTERFACE
+      "${IMGUI_DIR}/imgui.cpp"
+      "${IMGUI_DIR}/imgui_demo.cpp"
+      "${IMGUI_DIR}/imgui_draw.cpp"
+      "${IMGUI_DIR}/imgui_widgets.cpp"
+      "${IMGUI_DIR}/imgui_tables.cpp"
+      "${IMGUI_DIR}/backends/imgui_impl_opengl3.cpp"
+      "${IMGUI_DIR}/backends/imgui_impl_glfw.cpp"
+    )
+    target_link_libraries(ImGui INTERFACE OpenGL::OpenGL GLFW::GLFW)
+    add_library(ImGui::ImGui ALIAS ImGui)
+  endif()
+  set(ImGui_FOUND TRUE)
+else()
+  set(ImGui_FOUND FALSE)
+endif()
